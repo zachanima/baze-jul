b580315ff35734227af63f708629fc7346b1af85
Zach Ewert <zachanima@gmail.com>
2011-09-30 15:07:32 +0200

    Customise admin shop form

--- app/admin/shops.rb
+++ app/admin/shops.rb
@@ -11,4 +11,19 @@ ActiveAdmin.register Shop do
     column :closes_on
     default_actions
   end
+
+  form do |f|
+    f.inputs 'Basic' do
+      f.input :name
+      f.input :slug, label: 'Link'
+      f.input :text
+    end
+    f.inputs 'Advanced' do
+      f.input :username_text
+      f.input :password_text, as: :string
+      f.input :opens_on
+      f.input :closes_on
+    end
+    f.buttons
+  end
 end
