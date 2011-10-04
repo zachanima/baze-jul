4ce7fd54469a3d35cb1e9253ede9b17fd24d0b5a
Zach Ewert <zachanima@gmail.com>
2011-10-05 01:19:05 +0200

    Refactor validations in shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,6 +2,6 @@ class Shop < ActiveRecord::Base
   has_many :customers, dependent: :destroy
   has_many :products, dependent: :destroy
 
-  validates :title, :slug, presence: true
-  validates_uniqueness_of :slug
+  validates :title, presence: true
+  validates :slug, presence: true, uniqueness: true
 end
