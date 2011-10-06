a1b3e265644c163f50aa9acf0a2081f80d83a787
Zach Ewert <zachanima@gmail.com>
2011-10-06 05:12:53 +0200

    Add recently completed tasks to dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,44 +1,25 @@
 ActiveAdmin::Dashboards.build do
-
   section 'Pending Tasks' do
-    table_for Task.pending do
+    table_for Task.pending.limit(10) do
+      column 'State' do |task|
+        status_tag task.status
+      end
       column :title
+      column 'Created' do |task|
+        distance_of_time_as_sentence_to_now(task.created_at)
+      end
     end
   end
 
-  # Define your dashboard sections here. Each block will be
-  # rendered on the dashboard in the context of the view. So just
-  # return the content which you would like to display.
-
-  # == Simple Dashboard Section
-  # Here is an example of a simple dashboard section
-  #
-  #   section "Recent Posts" do
-  #     ul do
-  #       Post.recent(5).collect do |post|
-  #         li link_to(post.title, admin_post_path(post))
-  #       end
-  #     end
-  #   end
-
-  # == Render Partial Section
-  # The block is rendered within the context of the view, so you can
-  # easily render a partial rather than build content in ruby.
-  #
-  #   section "Recent Posts" do
-  #     div do
-  #       render 'recent_posts' # => this will render /app/views/admin/dashboard/_recent_posts.html.erb
-  #     end
-  #   end
-
-  # == Section Ordering
-  # The dashboard sections are ordered by a given priority from top left to
-  # bottom right. The default priority is 10. By giving a section numerically lower
-  # priority it will be sorted higher. For example:
-  #
-  #   section "Recent Posts", :priority => 10
-  #   section "Recent User", :priority => 1
-  #
-  # Will render the "Recent Users" then the "Recent Posts" sections on the dashboard.
-
+  section 'Recently Completed Tasks' do
+    table_for Task.complete.limit(10) do
+      column 'State' do |task|
+        status_tag task.status
+      end
+      column :title
+      column 'Completed' do |task|
+        distance_of_time_as_sentence_to_now(task.updated_at)
+      end
+    end
+  end
 end
