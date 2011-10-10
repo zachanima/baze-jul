4bfcb3acb9947ab82f34bdb7e464cf33e5ea6947
Zach Ewert <zachanima@gmail.com>
2011-10-10 03:41:23 +0200

    Make variants, variations dependency destroyed

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,6 +1,6 @@
 class Option < ActiveRecord::Base
   belongs_to :option_array, inverse_of: :options
-  has_many :variations
+  has_many :variations, dependent: :destroy
   has_many :products, through: :variations
 
   validates :text, :option_array, presence: true
--- app/models/option_array.rb
+++ app/models/option_array.rb
@@ -1,6 +1,6 @@
 class OptionArray < ActiveRecord::Base
   has_many :options, dependent: :destroy, inverse_of: :option_array
-  has_many :variants
+  has_many :variants, dependent: :destroy
   has_many :products, through: :variants
 
   accepts_nested_attributes_for :options, reject_if: :all_blank,
