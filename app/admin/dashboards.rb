ac2c70e0ee769a6256713a57c1549059a76516c2
Zach Ewert <zachanima@gmail.com>
2011-10-17 10:45:24 +0200

    Add recent orders, customers to dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,4 +1,24 @@
 ActiveAdmin::Dashboards.build do
+  section 'Recent Orders', priority: 1 do
+    table_for Order.order('created_at DESC').limit(10) do
+      column :shop
+      column :customer
+      column :product
+      column 'Placed' do |order|
+        distance_of_time_as_sentence_to_now(order.created_at)
+      end
+    end
+    strong { link_to 'View Orders', admin_orders_path }
+  end
+
+  section 'Customers in the last 10 minutes', priority: 2 do
+    table_for Customer.where('current_sign_in_at >= ?', Time.now - 600) do
+      column :shop
+      column :name
+    end
+    strong { link_to 'View Customers', admin_customers_path }
+  end
+
   section 'Pending Tasks' do
     table_for Task.pending.limit(10) do
       column 'State' do |task|
