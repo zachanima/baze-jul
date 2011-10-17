52662ad03db19b176bf1485d9024e39dbbedf4f1
Zach Ewert <zachanima@gmail.com>
2011-10-17 10:02:00 +0200

    Add import csv method to admin customers sidebar

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -26,4 +26,26 @@ ActiveAdmin.register Customer do
     end
     f.buttons
   end
+
+  collection_action :import_csv, method: :post do
+    @customers = CSV.parse(params[:file].read)
+    fields = @customers.first.collect { |f| f.underscore.to_sym }
+    @customers = @customers.drop(1).collect do |c|
+      customer = Hash.new
+      fields.each_index do |i|
+        customer[fields[i]] = c[i]
+      end
+      customer[:shop_id] = params[:user][:shop_id]
+      Customer.create(customer)
+    end
+    redirect_to action: :index, notice: 'CSV imported successfully!'
+  end
+
+  sidebar :import_csv do
+    form_tag import_csv_admin_customers_path, multipart: true do
+      (collection_select :user, :shop_id, Shop.all, :id, :title) +
+      (file_field_tag :file) +
+      submit_tag
+    end
+  end
 end
